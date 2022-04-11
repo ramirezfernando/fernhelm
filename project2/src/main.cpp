f5112844048ea4246fc978ab313e23e6c3f17908
Fernando <ramirez.fernando2003@gmail.com>
2022-04-10 20:17:44 -0500

    sdl window finally opens

--- project2/src/main.cpp
+++ project2/src/main.cpp
@@ -1,21 +1,39 @@
-#include <iostream>
-//#include <deez>
-//#include "SDL2.framework/Headers/SDL.h"
 #include <SDL2/SDL.h>
+#include <iostream>
 
 using namespace std;
 
-int main()
-{
+int main() {
     
-    if (SDL_Init(SDL_INIT_EVERYTHING) > 0)
-    {
-        cout << "SDL_Init failed with error: " << SDL_GetError() << endl;
-        return 1;
-    }
-    
-    cout << "Test" << endl;
-    
-    return EXIT_SUCCESS;    
+    SDL_Window * window = nullptr;
+
+    SDL_Surface * window_surface = nullptr;
+    SDL_Surface * image_surface = nullptr;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow("Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+
+    window_surface = SDL_GetWindowSurface(window);
+    image_surface = SDL_LoadBMP("image.bmp");
+
+    SDL_BlitSurface(image_surface, NULL, window_surface, NULL);
+
+    SDL_UpdateWindowSurface(window);
+
+    bool is_running = true;
+    SDL_Event event;
+    while (is_running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                is_running = false;
+            }
+        }
+    SDL_Delay(16);
 }
 
+    SDL_DestroyWindow(window);
+    SDL_FreeSurface(image_surface);
+    SDL_Quit();
+    return 0;
+}
